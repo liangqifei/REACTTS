1761ba1c1d3f07393e5594649485506c06134c6d
lqf <1454554357@qq.com>
2020-08-24 02:30:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var http=require('http'); //用来启服务
-var fs=require('fs'); //用来读取文件
-
-
-//开启服务
-var server=http.createServer().listen(3000); //端口号
-console.log('服务器开启成功');
+var http = require("http");
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        "Content-Type" : "text/plain;charset=utf-8" // 输出类型
+    });
+    response.write("你好");// 页面输出
+    response.end();
+}).listen(3000); // 监听端口号
+console.log("服务器启动!");
